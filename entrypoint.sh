208ec02a76b5b9931f7a8c6bc254ded3c009ff7f
diagutierrezro <diagutierrezro@unal.edu.co>
2026-05-04 20:18:50 -0500

    feat: Ajuste en versión de node

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-set -e
-set -u
-set -o pipefail
-
-export PLAN_TRABAJO_DOCENTE_CRUD_USER="$(aws ssm get-parameter --name /${PARAMETER_STORE}/plan_trabajo_docente_crud/db/username --output text --query Parameter.Value)"
-export PLAN_TRABAJO_DOCENTE_CRUD_PASS="$(aws ssm get-parameter --with-decryption --name /${PARAMETER_STORE}/plan_trabajo_docente_crud/db/password --output text --query Parameter.Value)"
+echo "Init app with PARAMETER_STORE: $PARAMETER_STORE"
 
 exec node dist/main
